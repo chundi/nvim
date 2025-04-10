b18d86b0495d6231acfb64bd146e6c47c369c81e
Chundi <songchundi@163.com>
2025-04-10 11:20:13 +0800

    update

--- lua/env.lua
+++ lua/env.lua
@@ -3,6 +3,6 @@ local os_name = vim.loop.os_uname().sysname
 if os_name == 'Darwin' then
   vim.env.JAVA_HOME = '/Library/Java/JavaVirtualMachines/temurin-21.jdk/Contents/Home'
 else
-  vim.env.JAVA_HOME = '/usr/lib/jvm/openjdk-21'
+  vim.env.JAVA_HOME = '/usr/lib64/jvm/java-21'
 end
 
